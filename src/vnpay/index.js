85897f5a6569fb6c4896501aa0db102ca2c1013c
Eric Tran <tung.tran@naustud.io>
2018-01-16 14:46:48 +0700

    refactor vnpay verifyReturnUrl function to be more useful

--- src/vnpay/index.js
+++ src/vnpay/index.js
@@ -3,27 +3,8 @@
  */
 
 /*eslint-disable no-shadow, key-spacing, no-param-reassign*/
-import SimpleSchema from 'simpl-schema';
 
 export { VNPay } from './VNPay';
-
-/* eslint-disable no-unused-vars */
-// for reference, not used for now
-const vnpayReturnSchema = new SimpleSchema(
-	{
-		vnp_TmnCode: String,
-		vnp_TxnRef: String,
-		vnp_Amount: Number,
-		vnp_OrderInfo: String,
-		vnp_ResponseCode: String,
-		vnp_BankCode: String,
-		vnp_BankTranNo: String,
-		vnp_PayDate: String,
-		vnp_TransactionNo: String,
-		vnp_SecureHash: String,
-	},
-	{ requiredByDefault: false }
-);
 
 const TEST_CONFIG = {
 	paymentGateway: 'http://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
