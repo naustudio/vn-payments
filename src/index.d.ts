83ea25d9e611c84bca19e8b0e3ac332fdb299ff7
Tu Nguyen <nguyendichtu91295@gmail.com>
2018-02-06 18:49:04 +0700

    Add .d.ts file to support autocompletion on VsCode

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { VNPay } from './vnpay';
+import { NganLuong } from './nganluong';
+import { OnpayDomestic, OnePayInternational } from './onepay';
+import { SohaPay } from './sogaoat';
+
+export { VNPay, NganLuong, OnpayDomestic, OnePayInternational, SohaPay };
